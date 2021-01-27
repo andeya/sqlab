49f2dd19cd226b68addbf77455114a0b320a33d8
henrylee2cn <henrylee2cn@gmail.com>
2021-01-27 14:56:34 +0800

    chore: update Mysql
    Change-Id: I2664d1f640b5679cfb10b60202a07e83cc5f363d

--- mysql.go
+++ mysql.go
@@ -11,36 +11,64 @@ type Mysql struct {
 	Sql
 }
 
-func (m Mysql) UpsertMaker(
+// UpsertBuilder build INSERT INTO ... ON DUPLICATE KEY UPDATE ...
+func (m Mysql) UpsertBuilder(
 	tableName string, fields []string,
 ) func(valueGroups ...[]interface{}) (sql string, values []interface{}, err error) {
-	if len(fields) == 0 {
+	if len(tableName) == 0 || len(fields) == 0 {
 		return func(valueGroups ...[]interface{}) (sql string, values []interface{}, err error) {
-			return "", nil, errors.New("no fields")
+			return "", nil, errors.New("no table name or no fields")
 		}
 	}
-	if !strings.HasPrefix(tableName, "`") {
-		tableName = "`" + tableName + "`"
+	buf := strings.Builder{}
+	buf.Grow(20 + len(tableName) + (len(fields) * 5)) // experience value
+	buf.WriteString("INSERT INTO ")
+	if tableName[0] != '`' {
+		buf.WriteByte('`')
+		buf.WriteString(tableName)
+		buf.WriteByte('`')
+	} else {
+		buf.WriteString(tableName)
 	}
-	updateFields := make([]string, len(fields))
+	buf.WriteString(" (")
+	buf2 := strings.Builder{}
+	buf2.Grow(30 + (len(fields)*6)*3) // experience value
+	buf2.WriteString(" ON DUPLICATE KEY UPDATE ")
 	for i, field := range fields {
 		if !strings.HasPrefix(field, "`") {
-			field = "`" + field + "`"
-			fields[i] = field
+			buf.WriteByte('`')
+			buf.WriteString(field)
+			buf.WriteByte('`')
+			buf2.WriteByte('`')
+			buf2.WriteString(field)
+			buf2.WriteString("`=VALUES(`")
+			buf2.WriteString(field)
+			buf2.WriteString("`)")
+		} else {
+			buf.WriteString(field)
+			buf2.WriteString(field)
+			buf2.WriteString("=VALUES(")
+			buf2.WriteString(field)
+			buf2.WriteString(")")
 		}
-		updateFields[i] = field + "=VALUES(" + field + ")"
+		if i < len(fields)-1 {
+			buf.WriteByte(',')
+			buf2.WriteByte(',')
+		}
 	}
-	var sqlTpl = fmt.Sprintf("INSERT INTO %s (%s) VALUES %%s ON DUPLICATE KEY UPDATE %s",
-		tableName,
-		strings.Join(fields, ","),
-		strings.Join(updateFields, ","),
-	)
+	buf.WriteString(") VALUES ")
+	segment1 := buf.String()
+	groupMark := "(" + strings.Repeat(",?", len(fields))[1:] + ")"
+	segment2 := buf2.String()
 	return func(valueGroups ...[]interface{}) (sql string, values []interface{}, err error) {
 		if len(valueGroups) == 0 {
 			return "", nil, errors.New("no data to update")
 		}
 		values = make([]interface{}, 0, len(fields)*len(valueGroups))
-		for _, vars := range valueGroups {
+		bufOne := strings.Builder{}
+		bufOne.Grow(len(segment1) + len(segment2) + (len(valueGroups) * (len(groupMark) + 1)))
+		bufOne.WriteString(segment1)
+		for i, vars := range valueGroups {
 			if len(vars) != len(fields) {
 				return "", nil, fmt.Errorf(
 					"number of values and fields is not equal: %d!=%d",
@@ -48,10 +76,13 @@ func (m Mysql) UpsertMaker(
 				)
 			}
 			values = append(values, vars...)
+			bufOne.WriteString(groupMark)
+			if i < len(valueGroups)-1 {
+				bufOne.WriteByte(',')
+			}
 		}
-		sql = fmt.Sprintf(sqlTpl,
-			strings.Repeat(",("+strings.Repeat(",?", len(fields))[1:]+")", len(valueGroups))[1:],
-		)
+		bufOne.WriteString(segment2)
+		sql = bufOne.String()
 		return sql, values, nil
 	}
 }
--- mysql_test.go
+++ mysql_test.go
@@ -6,9 +6,20 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestUpsertMaker(t *testing.T) {
-	maker := Mysql{}.UpsertMaker("table1", []string{"id", "status", "deleted"})
+func TestUpsertBuilder(t *testing.T) {
+	maker := Mysql{}.UpsertBuilder("table1", []string{"id", "status", "deleted"})
+
 	sql, values, err := maker(
+		[]interface{}{1, "succ", true},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t,
+		"INSERT INTO `table1` (`id`,`status`,`deleted`) VALUES (?,?,?) ON DUPLICATE KEY UPDATE `id`=VALUES(`id`),`status`=VALUES(`status`),`deleted`=VALUES(`deleted`)",
+		sql,
+	)
+	assert.Equal(t, []interface{}{1, "succ", true}, values)
+
+	sql, values, err = maker(
 		[]interface{}{1, "succ", true},
 		[]interface{}{2, "fail", false},
 	)
@@ -18,4 +29,16 @@ func TestUpsertMaker(t *testing.T) {
 		sql,
 	)
 	assert.Equal(t, []interface{}{1, "succ", true, 2, "fail", false}, values)
+
+	sql, values, err = maker(
+		[]interface{}{3, "succ2", false},
+		[]interface{}{4, "fail2", false},
+		[]interface{}{5, "fail2", false},
+	)
+	assert.NoError(t, err)
+	assert.Equal(t,
+		"INSERT INTO `table1` (`id`,`status`,`deleted`) VALUES (?,?,?),(?,?,?),(?,?,?) ON DUPLICATE KEY UPDATE `id`=VALUES(`id`),`status`=VALUES(`status`),`deleted`=VALUES(`deleted`)",
+		sql,
+	)
+	assert.Equal(t, []interface{}{3, "succ2", false, 4, "fail2", false, 5, "fail2", false}, values)
 }
